8f9220ae35d0c737c72526f4932644cd83c5920f
CaptainGlac1er <georgecolgrove@outlook.com>
2018-08-12 18:25:34 -0500

    updated dependencies

---
+++ src/service/experience.service.spec.ts
@@ -0,0 +1,15 @@
+import {inject, TestBed} from '@angular/core/testing';
+
+import {ExperienceService} from './experience.service';
+
+describe('ExperienceService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ExperienceService]
+    });
+  });
+
+  it('should be created', inject([ExperienceService], (service: ExperienceService) => {
+    expect(service).toBeTruthy();
+  }));
+});
