0dbf398b8ab7f64c7c3572408980c2cc2d5af66d
George <georgecolgrove@outlook.com>
2018-08-26 01:00:27 -0500

    Made nav bar responsive with mobile navigation menu (#6)
    * Lotta updates
    
    * updated package file
    
    * Lotta updates
    
    * updated package file
    
    * updated package file
    
    * improved UI and fixes
    
    * Worked on skeleton project page and core refractoring
    
    * made nav bar responsive and added mobile menu
    
    * small improvements

---
+++ src/service/mobile.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MobileService {
+  public hamburgerShown: boolean;
+  constructor() { }
+  closeHamburgerMenu() {
+    this.hamburgerShown = false;
+  }
+  toggleHamburgerMenu() {
+    this.hamburgerShown = !this.hamburgerShown;
+  }
+}
