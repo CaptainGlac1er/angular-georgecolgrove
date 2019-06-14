6aa6d06e1083213cd6bd85325cd8e5f87d71c111
George Walter Colgrove IV <georgecolgrove@outlook.com>
2019-06-13 23:05:28 -0400

    changed site theme updated homepage

--- src/components/home-page/components/home-page/home-page.component.ts
+++ src/components/home-page/components/home-page/home-page.component.ts
@@ -1,16 +1,34 @@
-import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
+import {Component, ElementRef, HostListener, Inject, OnDestroy, OnInit, PLATFORM_ID, ViewChild} from '@angular/core';
 import { Job } from '../../../../classes/job';
 import { Router } from '@angular/router';
 import { Meta, Title } from '@angular/platform-browser';
 import { ExperienceService } from '@service/experience.service';
-import { isPlatformBrowser } from '@angular/common';
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
 import { environment } from '../../../../environments/environment';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
-  providers: [ExperienceService]
+  providers: [ExperienceService],
+  animations: [
+    trigger('nameVisible', [
+      state('visible', style({
+        flex: '1 0 200px',
+      })),
+      state('hidden', style({
+        flex: '0',
+        width: '0'
+      })),
+      transition('visible => hidden', [
+        animate('.2s')
+      ]),
+      transition('hidden => visible', [
+        animate('0.2s')
+      ]),
+    ])
+  ]
 })
 export class HomePageComponent implements OnInit {
   isBrowser: boolean;
@@ -18,6 +36,7 @@ export class HomePageComponent implements OnInit {
   name = 'George Walter Colgrove IV';
   gamertag = 'CaptainGlac1er';
   phone = '+1 802 595-1798';
+  isMinimized: boolean = false;
   about = {
     title: 'About:',
     text: 'Constructing well built secure software is what I strive for. <br />'
@@ -50,11 +69,14 @@ export class HomePageComponent implements OnInit {
     }
   };
 
+  @ViewChild('header', {static: false}) titleElem: ElementRef;
+  nameVisible: string;
   constructor(
     private experienceService: ExperienceService,
     private router: Router,
     private meta: Meta,
     private titleService: Title,
+    @Inject(DOCUMENT) private document: Document,
     @Inject(PLATFORM_ID) platformId: Object) {
     this.isBrowser = isPlatformBrowser(platformId);
   }
@@ -64,5 +86,13 @@ export class HomePageComponent implements OnInit {
       this.experienceService.getExperiencesData().then(item => this.jobs = item);
     }
     this.titleService.setTitle('George Walter Colgrove IV - Personal Website');
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isMinimized = document.documentElement.scrollTop > 0;
+    const height = Math.max(0, 300 - window.scrollY);
+    this.titleElem.nativeElement.style.height  = height + 'px';
+    console.log(height);
+    this.nameVisible = height === 0 ? 'visible' : 'hidden';
   }
 }
