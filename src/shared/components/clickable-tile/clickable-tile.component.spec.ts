c258a65236ee00b24522b28da920c6d2a107f94e
George Walter Colgrove IV <georgecolgrove@outlook.com>
2022-05-26 22:06:48 -0400

    initial angular 11 upgrade

--- src/shared/components/clickable-tile/clickable-tile.component.spec.ts
+++ src/shared/components/clickable-tile/clickable-tile.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ClickableTileComponent } from './clickable-tile.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -8,7 +8,7 @@ describe('ClickableTileComponent', () => {
   let component: ClickableTileComponent;
   let fixture: ComponentFixture<ClickableTileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
--- src/shared/components/pretty-tile/pretty-tile.component.spec.ts
+++ src/shared/components/pretty-tile/pretty-tile.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PrettyTileComponent } from './pretty-tile.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -7,7 +7,7 @@ describe('PrettyTileComponent', () => {
   let component: PrettyTileComponent;
   let fixture: ComponentFixture<PrettyTileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PrettyTileComponent ],
       schemas: [
